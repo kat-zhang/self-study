2cc4c5c9c709c33708eaca9422683bbebaa1a4b4
kat zhang <kzhang193@gmail.com>
2019-02-20 14:00:52 -0600

    title function

--- front-end-dev-book/ottergram/scripts/main.js
+++ front-end-dev-book/ottergram/scripts/main.js
@@ -3,7 +3,12 @@ var titleSelector = "[data-role='title']";
 var linkSelector = "[data-role='trigger']";
 
 function setDetails() {
+    // Change the detail image
     var detailImage = document.querySelector(imageSelector);
     detailImage.setAttribute("src", "images/otter3.jpg");
+
+    //Change the detail image title
+    var detailTitle = document.querySelector(titleSelector);
+    detailTitle.textContent = "You Should Be Dancing";
 
 }
