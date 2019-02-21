1241e0b35cf5ee4fd3878945ae1ea066df0da745
kat zhang <kzhang193@gmail.com>
2019-02-20 23:38:47 -0600

    idk why something is broken

--- front-end-dev-book/ottergram/scripts/main.js
+++ front-end-dev-book/ottergram/scripts/main.js
@@ -2,13 +2,40 @@ var imageSelector =  "[data-role='target']";
 var titleSelector = "[data-role='title']";
 var linkSelector = "[data-role='trigger']";
 
-function setDetails() {
+function setDetails(imageUrl, titleText) {
     // Change the detail image
+    "use strict"
     var detailImage = document.querySelector(imageSelector);
-    detailImage.setAttribute("src", "images/otter3.jpg");
+    detailImage.setAttribute("src", imageUrl);
 
     //Change the detail image title
     var detailTitle = document.querySelector(titleSelector);
-    detailTitle.textContent = "You Should Be Dancing";
+    detailTitle.textContent = titleText;
 
+}
+
+///Get the information from the thumbnails to display in the main image
+function getImage(thumbnail) {
+    "use strict";
+    return thumbnail.getAttribute("data-url");
+}
+
+function getTitle(thumbnail) {
+    "use strict";
+    return thumbnail.getAttribute("data-title");
+
+}
+
+function getDetails(thumbnail) {
+    "use strict";
+    setDetails(getImage(thumbnail), getTitle(thumbnail));
+}
+
+function addThumbClickHandler(thumb) {
+    "use strict";
+    thumbnail.addEventListener("click", function(event) {
+        event.preventDefault();
+        setDetails(thumb)
+    
+    });
 }
