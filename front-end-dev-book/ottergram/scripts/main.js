f4d709f18f3f18cc131ceba209259f71426cc188
kat zhang <kzhang193@gmail.com>
2019-02-23 01:01:06 -0600

    i give up

--- front-end-dev-book/ottergram/scripts/main.js
+++ front-end-dev-book/ottergram/scripts/main.js
@@ -4,39 +4,103 @@ const titleSelector = "[data-role='title']";
 const linkSelector = "[data-role='trigger']";
 
 function setDetails(imageUrl, titleText) {
-    // Change the detail image
-    "use strict"
+    'use strict';
+  
     var detailImage = document.querySelector(imageSelector);
-    detailImage.setAttribute("src", imageUrl);
-
-    //Change the detail image title
+    detailImage.setAttribute('src', imageUrl);
+  
     var detailTitle = document.querySelector(titleSelector);
     detailTitle.textContent = titleText;
-
-}
-
-///Get the information from the thumbnails to display in the main image
-function getImage(thumbnail) {
-    "use strict";
-    return thumbnail.getAttribute("data-url");
-}
-
-function getTitle(thumbnail) {
-    "use strict";
-    return thumbnail.getAttribute("data-title");
-
-}
-
-function getDetails(thumbnail) {
-    "use strict";
-    setDetails(getImage(thumbnail), getTitle(thumbnail));
-}
-
-function addThumbClickHandler(thumb) {
-    "use strict";
-    thumbnail.addEventListener("click", function(event) {
-        event.preventDefault();
-        setDetails(thumb)
-    
+  }
+  
+  function imageFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-url');
+  }
+  
+  function titleFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-title')
+  }
+  
+  function setDetailsFromThumb(thumbnail) {
+    'use strict';
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+  }
+  
+  function addThumbClickHandler(thumb) {
+    thumb.addEventListener('click', function (event) {
+      event.preventDefault();
+      setDetailsFromThumb(thumb);
+      showDetails();
     });
-}
+  }
+  
+  function getThumbnailsArray() {
+    'use strict';
+    var thumbnails = document.querySelectorAll(linkSelector);
+    var thumbnailArray = [].slice.call(thumbnails);
+    return thumbnailArray;
+  }
+  
+  
+  function initializeEvents() {
+    'use strict';
+    var thumbnails = getThumbnailsArray();
+    thumbnails.forEach(addThumbClickHandler);
+  }
+  
+  initializeEvents();
+  
+// function setDetails(imageUrl, titleText) {
+//     // Change the detail image
+//     "use strict"
+//     var detailImage = document.querySelector(imageSelector);
+//     detailImage.setAttribute("src", imageUrl);
+
+//     //Change the detail image title
+//     var detailTitle = document.querySelector(titleSelector);
+//     detailTitle.textContent = titleText;
+
+// }
+
+// ///Get the information from the thumbnails to display in the main image
+// function getImage(thumbnail) {
+//     "use strict";
+//     return thumbnail.getAttribute("data-url");
+// }
+
+// function getTitle(thumbnail) {
+//     "use strict";
+//     return thumbnail.getAttribute("data-title");
+
+// }
+
+// //Gets the details from the thumbnails and sets them to the main image
+// function getDetails(thumbnail) {
+//     "use strict";
+//     setDetails(getImage(thumbnail), getTitle(thumbnail));
+// }
+
+// function addThumbClickHandler(thumb) {
+//     "use strict";
+//     thumb.addEventListener('click', function (event) {
+//         event.preventDefault();
+//         getDetails(thumb);
+//     });
+// }
+
+// function getThumbnailsArray() {
+//     "use strict";
+//     var thumbnails = document.querySelectorAll(linkSelector);
+//     var thumbnailArray = [].slice.call(thumbnails);
+//     return thumbnailArray;
+// }
+
+// function initializeEvents() {
+//     "use strict";
+//     var thumbnails = getThumbnailsArray();
+//     thumbnails.forEach(addThumbClickHandler);
+// }
+
+// initializeEvents();
